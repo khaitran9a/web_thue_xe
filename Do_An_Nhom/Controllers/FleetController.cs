2af6ce42d9de6c721c13986cc91b6bc97af0f5b8
Tran Van Khai <tranvankhai17042000@gmail.com>
2022-11-21 15:20:14 +0700

    update 21/11/2022

---
+++ Do_An_Nhom/Controllers/FleetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Do_An_Nhom.Controllers
+{
+    public class FleetController : Controller
+    {
+        // GET: Fleet
+        public ActionResult FleetView()
+        {
+            return View();
+        }
+    }
+}
---
+++ Do_An_Nhom/Controllers/OfferController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Do_An_Nhom.Controllers
+{
+    public class OfferController : Controller
+    {
+        // GET: Offer
+        public ActionResult OfferView()
+        {
+            return View();
+        }
+    }
+}
